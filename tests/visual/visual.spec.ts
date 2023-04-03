0e25807b9488b7ae6a2e1ff22789284d38edaea3
danielklapp <daniel@klapp.name>
2023-04-03 16:42:05 -0400

    Created regression testing for GWE

---
+++ tests/visual/visual.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test.describe.only("Visual Regression Testing GWE", () => {
+  test("Full Page Snapshot", async ({ page }) => {
+    await page.goto(`${process.env.BASE_URL}?server=QA`);
+    expect(await page.screenshot()).toMatchSnapshot("homepage.png");
+  });
+
+  test("Single Element Snapshop", async ({ page }) => {
+    await page.goto(`${process.env.BASE_URL}?server=QA`);
+    const pageElement = await page.$("h1");
+    expect(await pageElement?.screenshot()).toMatchSnapshot("page-title.png");
+  });
+});
