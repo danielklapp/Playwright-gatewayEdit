82e9f3d6202cffaee1bb7f4362d9d6803bd1513a
danielklapp <daniel@klapp.name>
2023-04-27 16:16:52 -0400

    test for checking prev and next buttons for tn

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -268,6 +268,48 @@ test.fixme("Align button for Scripture cards", async ({ page }) => {
   await page.locator("#alignment_icon_TARGET_SIMPLIFIED").click();
   await page.getByRole("button", { name: "Cancel" }).click();
 });
+
+test("Checking previous and next buttons for translationNotes", async ({
+  page,
+}) => {
+  await page.goto(
+    "https://release-v2-0-0--gateway-edit.netlify.app/?server=QA"
+  );
+  await page
+    .locator('[data-test="username-input"]')
+    .getByRole("textbox")
+    .click();
+  await page
+    .locator('[data-test="username-input"]')
+    .getByRole("textbox")
+    .fill(`${process.env.USERNAME}`);
+  await page
+    .locator('[data-test="password-input"]')
+    .getByRole("textbox")
+    .click();
+  await page
+    .locator('[data-test="password-input"]')
+    .getByRole("textbox")
+    .fill(`${process.env.PASSWORD}`);
+  await page.getByLabel("Keep me logged in").check();
+  await page.locator('[data-test="submit-button"]').click();
+  await page.locator("#organization-select-outlined").click();
+  await page.getByRole("option", { name: "unfoldingWord" }).click();
+  await page.getByRole("button", { name: "​", exact: true }).click();
+  await page.getByRole("option", { name: "en - English - English" }).click();
+  await page.getByRole("button", { name: "Save and Continue" }).click();
+  await page.locator("#resource_card_tn_prev").click();
+  await page.locator("#resource_card_tn_prev").click();
+  await page.locator("#resource_card_tn_prev").click();
+  await page.locator("#resource_card_tn_prev").click();
+  await page.locator("#resource_card_tn_prev").click();
+  await page.locator("#resource_card_tn_next").click();
+  await page.locator("#resource_card_tn_next").click();
+  await page.locator("#resource_card_tn_next").click();
+  await page.locator("#resource_card_tn_next").click();
+  await page.locator("#resource_card_tn_next").click();
+});
+
 /*
 test("Slider test one", async ({ page }) => {
   await page.goto(`${process.env.BASE_URL}?server=QA`);
