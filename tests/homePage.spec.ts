dd7affa67dfb18df8d56e4d631dbfbf953618263
danielklapp <daniel@klapp.name>
2023-04-27 16:43:08 -0400

    test for prev and next buttons for twa.

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -310,6 +310,45 @@ test("Checking previous and next buttons for translationNotes", async ({
   await page.locator("#resource_card_tn_next").click();
 });
 
+test("Checking previous and next buttons for translationWords Articles", async ({
+  page,
+}) => {
+  await page.goto(
+    "https://release-v2-0-0--gateway-edit.netlify.app/?server=QA"
+  );
+  await page
+    .locator('[data-test="username-input"]')
+    .getByRole("textbox")
+    .click();
+  await page
+    .locator('[data-test="username-input"]')
+    .getByRole("textbox")
+    .fill(`${process.env.USERNAME}`);
+  await page
+    .locator('[data-test="password-input"]')
+    .getByRole("textbox")
+    .click();
+  await page
+    .locator('[data-test="password-input"]')
+    .getByRole("textbox")
+    .fill(`${process.env.PASSWORD}`);
+  await page.getByLabel("Keep me logged in").check();
+  await page.locator('[data-test="submit-button"]').click();
+  await page.locator("#organization-select-outlined").click();
+  await page.getByRole("option", { name: "unfoldingWord" }).click();
+  await page.getByRole("button", { name: "​", exact: true }).click();
+  await page.getByRole("option", { name: "en - English - English" }).click();
+  await page.getByRole("button", { name: "Save and Continue" }).click();
+  // await page.pause();
+  await page.locator("#resource_card_twa_prev").click();
+  await page.locator("#resource_card_twa_prev").click();
+  await page.locator("#resource_card_twa_prev").click();
+  await page.locator("#resource_card_twa_prev").click();
+  await page.locator("#resource_card_twa_next").click();
+  await page.locator("#resource_card_twa_next").click();
+  await page.locator("#resource_card_twa_next").click();
+  await page.locator("#resource_card_twa_next").click();
+});
 /*
 test("Slider test one", async ({ page }) => {
   await page.goto(`${process.env.BASE_URL}?server=QA`);
