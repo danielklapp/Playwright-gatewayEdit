b21fd07552f26a7a6a0b079eb71906a78434f861
danielklapp <daniel@klapp.name>
2023-05-14 16:55:20 -0400

    Added assertion test for save button.

--- tests/assertions.spec.ts
+++ tests/assertions.spec.ts
@@ -86,3 +86,49 @@ test("Title of markdown / preview button on cards", async ({ page }) => {
   const previewTitle_tq = page.locator("#markdown_button_resource_card_tq");
   await expect(previewTitle_tq).toHaveAttribute("title", "Preview");
 });
+
+test.fixme(
+  "Save button is disabled when there's no edit to card",
+  async ({ page }) => {
+    await page.goto(
+      "https://release-v2-0-0--gateway-edit.netlify.app/?server=QA"
+    );
+    await page
+      .locator('[data-test="username-input"]')
+      .getByRole("textbox")
+      .click();
+    await page
+      .locator('[data-test="username-input"]')
+      .getByRole("textbox")
+      .fill(`${process.env.USERNAME}`);
+    await page
+      .locator('[data-test="password-input"]')
+      .getByRole("textbox")
+      .click();
+    await page
+      .locator('[data-test="password-input"]')
+      .getByRole("textbox")
+      .fill(`${process.env.PASSWORD}`);
+    await page.getByLabel("Keep me logged in").check();
+    await page.locator('[data-test="submit-button"]').click();
+    await page.locator("#organization-select-outlined").click();
+    await page.getByRole("option", { name: "unfoldingWord" }).click();
+    await page.getByRole("button", { name: "​", exact: true }).click();
+    await page.getByRole("option", { name: "en - English - English" }).click();
+    await page.getByRole("button", { name: "Save and Continue" }).click();
+    const saveButton_0 = page.locator("#save_button_scripture_card_0");
+    await expect(saveButton_0).toBeDisabled();
+    const saveButton_2 = page.locator("#save_button_scripture_card_2");
+    await expect(saveButton_2).toBeDisabled();
+    const saveButton_tn = page.locator("#save_button_resource_card_tn");
+    await expect(saveButton_tn).toBeDisabled();
+    const saveButton_ta = page.locator("#save_button_resource_card_ta");
+    await expect(saveButton_ta).toBeDisabled();
+    const saveButton_twl = page.locator("#save_button_resource_card_twl");
+    await expect(saveButton_twl).toBeDisabled();
+    const saveButton_twa = page.locator("#save_button_resource_card_twa");
+    await expect(saveButton_twa).toBeDisabled();
+    const saveButton_tq = page.locator("#save_button_resource_card_tq");
+    await expect(saveButton_tq).toBeDisabled();
+  }
+);
