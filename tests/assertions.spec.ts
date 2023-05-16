73a55f5d557bbb646266c04e00f60793154d462d
danielklapp <daniel@klapp.name>
2023-05-16 14:54:24 -0400

    Direct address to QA.

--- tests/assertions.spec.ts
+++ tests/assertions.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe.parallel("All my assertion tests together", () => {
   test.beforeEach(async ({ page }) => {
-    // await page.goto(`${process.env.BASE_URL}`);
+    // await page.goto(`${process.env.BASE_URL}?server=QA`);
     await page.goto(
       "https://release-v2-0-0--gateway-edit.netlify.app/?server=QA"
     );
