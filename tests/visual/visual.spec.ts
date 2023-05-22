02ede0edc640a44138f5d7f4a864358df7b030bc
danielklapp <daniel@klapp.name>
2023-05-22 14:47:18 -0400

    took out .only

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.describe.only("Visual Regression Testing GWE", () => {
+test.describe("Visual Regression Testing GWE", () => {
   test("Full Page Snapshot", async ({ page }) => {
     await page.goto(`${process.env.BASE_URL}?server=QA`);
     expect(await page.screenshot()).toMatchSnapshot("homepage.png");
