d4edf432c31354c148b84a9b4e45dbe5cac982ae
danielklapp <daniel@klapp.name>
2023-05-24 22:32:34 -0400

    Added AccountSettingsPage and assertion tests.

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,14 +1,17 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../page-objects/LoginPage";
 import { HomePage } from "../../page-objects/HomePage";
+import { AccountSettingsPage } from "../../page-objects/AccountSettingsPage";
 
 test.describe.parallel("Login / Logout Flow", () => {
   let loginPage: LoginPage;
   let homePage: HomePage;
+  let accountSettingsPage: AccountSettingsPage;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
     homePage = new HomePage(page);
+    accountSettingsPage = new AccountSettingsPage(page);
 
     await homePage.visit();
   });
@@ -17,10 +20,24 @@ test.describe.parallel("Login / Logout Flow", () => {
     await loginPage.login();
   });
 
+  test("Checking to make sure you have logged in", async ({ page }) => {
+    await loginPage.login();
+    await accountSettingsPage.translationSettings();
+    await loginPage.reallyLoggedin();
+  });
+
   test("Logout for gatewayEdit", async ({ page }) => {
     // Must login before logout - due to test isolation
     await loginPage.login();
+    await accountSettingsPage.translationSettings();
     await loginPage.logout();
+  });
+
+  test.only("Checking to make sure you have logged Out", async ({ page }) => {
+    await loginPage.login();
+    await accountSettingsPage.translationSettings();
+    await loginPage.logout();
+    await loginPage.reallyLoggedout();
   });
 
   test("Trying to login with wrong username and password", async ({ page }) => {
