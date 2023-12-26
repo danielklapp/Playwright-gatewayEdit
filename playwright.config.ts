576a0c6a0e7d4cf03a094f68c6bc30e350dc215f
danielklapp <daniel@klapp.name>
2023-12-26 16:56:01 -0500

    Added project dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,8 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 import * as dotenv from "dotenv";
-const { devices } = require("@playwright/test");
+import { devices } from "@playwright/test";
+
+export const STORAGE_STATE_PATH = "./loginAuth.json";
 
 dotenv.config({
   path: "./.env",
@@ -10,6 +12,7 @@ const config: PlaywrightTestConfig = {
   timeout: 60000,
   retries: 0,
   testDir: "./tests",
+  fullyParallel: false,
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
@@ -27,16 +30,33 @@ const config: PlaywrightTestConfig = {
 
   projects: [
     {
+      name: "setup",
+      testDir: "./",
+      testMatch: "global-setup.ts",
+    },
+    {
       name: "Chromium",
-      use: { browserName: "chromium", ignoreHTTPSErrors: true },
+      dependencies: ["setup"],
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: STORAGE_STATE_PATH,
+      },
     },
     {
       name: "Firefox",
-      use: { browserName: "firefox" },
+      dependencies: ["setup"],
+      use: {
+        ...devices["Desktop Firefox"],
+        storageState: STORAGE_STATE_PATH,
+      },
     },
     {
       name: "Webkit",
-      use: { browserName: "webkit" },
+      dependencies: ["setup"],
+      use: {
+        ...devices["Desktop Webkit"],
+        storageState: STORAGE_STATE_PATH,
+      },
     },
   ],
 };
